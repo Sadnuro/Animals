d92217116076b6412eb32eaae8582b3ee2506a49
sadith.nunez <sadnr0213@gmail.com>
2021-10-05 21:58:41 -0500

    functional project sprint 1

---
+++ scripts/form5.js
@@ -0,0 +1,26 @@
+function validar_confirmar_correo (original, toConfirm) {
+    // Validate format | regex
+    if (/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(original) 
+    &&  /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(toConfirm) ) {
+        // Verify mails
+        if(original === toconfirm){
+            return true;
+        }
+    }
+    return false;
+}
+
+function validar_pais (pais) {
+    // Check string
+    if (/^[A-Z]+[a-z ]+$/.test(pais)){
+        return true;
+    }
+    return false;
+}
+
+module.exports.validar_confirmar_correo = validar_confirmar_correo;
+module.exports.validar_pais = validar_pais;
+
+
+    // 2 == "2" > true
+    // 2 === 2 > true
